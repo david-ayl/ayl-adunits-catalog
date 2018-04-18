416df12c6fc0662768ee8482c4fc5d5138a78a59
dvdtsr <tessier.dvd@gmail.com>
2018-04-18 22:22:45 +0200

    feat(responsive wip)

--- app/scripts/src/_includes/toggle.js
+++ app/scripts/src/_includes/toggle.js
@@ -2,6 +2,22 @@
 
 $(document).on("click", "#page_switcher", function() {
 
-  console.log($("#iframe").contents().find("body").toggleClass("article_view").toggleClass("hp_view"));
+  function getCurrentHeight() {
+
+    var ifr = $("#iframe").contents();
+    var currentHeight;
+    if( ifr.find(".hp_view #hp_page").length ) {
+      currentHeight = ifr.find(".hp_view #hp_page").height();
+    }
+    else if( ifr.find(".article_view #article_page").length ) {
+      currentHeight = ifr.find(".article_view #article_page").height();
+    }
+    return currentHeight;
+
+  }
+
+  $("#iframe").contents().find("body").toggleClass("article_view").toggleClass("hp_view");
+  $("#iframe").contents().find(".site_sizer").height("auto");
+  $("#iframe").contents().find(".site_sizer").height(getCurrentHeight() + "px");
 
 })
