445bd996f327e12b6b0d7b1e4d2863111a8325b3
david <david@adyoulike.com>
2018-04-26 22:53:39 +0200

    feat(highlight n zoom)

--- app/scripts/src/main.js
+++ app/scripts/src/main.js
@@ -1,4 +1,23 @@
 
 $(document).ready(function() {
 
+  var _$ = function(elem) {
+    return $("#iframe").contents().find(elem);
+  }
+
+
+  $("#change_zoom").on("change", function() {
+    _$("body").animate({"zoom" : $(this).val() / 10}, "slow");
+  });
+
+  $(".checkbox_input").on("change", function() {
+    var context = $(this).attr("name");
+    if ( $(this).prop("checked") ) {
+      _$("[data-adunit='" + context + "']").addClass("highlighted")
+    }
+    else {
+      _$("[data-adunit='" + context + "']").removeClass("highlighted")
+    }
+  })
+
 });
