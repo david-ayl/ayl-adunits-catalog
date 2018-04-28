18ed4297c0a630685dd6c597eacf9a4ee334c0cd
david <david@adyoulike.com>
2018-04-28 22:28:11 +0200

    feat(pagination)

--- app/scripts/src/main.js
+++ app/scripts/src/main.js
@@ -4,6 +4,26 @@ $(document).ready(function() {
   $(function() {
     $.scrollify({
       section : ".section",
+      scrollbars : false,
+      before : function(i,panels) {
+        var ref = panels[i].attr("data-section-name");
+        $(".pagination .active").removeClass("active");
+        $(".pagination").find("a[href=\"#" + ref + "\"]").addClass("active");
+      },
+      afterRender : function() {
+        var pagination = "<ul class=\"pagination\">";
+        var activeClass = "";
+        $(".section").each(function(i) {
+          activeClass = "";
+          if(i===0) {
+            activeClass = "active";
+          }
+          pagination += "<li class=\"pagination-" + $(this).attr("data-section-name") + "\"><a class=\"" + activeClass + "\" href=\"#" + $(this).attr("data-section-name") + "\"><span class=\"hover-text\">" + $(this).attr("data-section-name").charAt(0).toUpperCase() + $(this).attr("data-section-name").slice(1) + "</span></a></li>";
+        });
+        pagination += "</ul>";
+        $("body").append(pagination);
+        $(".pagination a").on("click",$.scrollify.move);
+      }
     });
   });
 
